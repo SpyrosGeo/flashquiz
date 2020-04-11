5d070eb2e77b79fe240be58fca8386cf6406b2eb
SpyrosGeo <xarodasimba@gmail.com>
2020-04-12 00:35:30 +0300

    added main components and created the flashcards basic css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React ,{useState} from 'react';
+import FlashcardList from './componets/FlashcardList';
+import './app.css'
 function App() {
+
+  const [flashcards,setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <FlashcardList flashcards={flashcards}/>
   );
 }
 
+const SAMPLE_FLASHCARDS=[
+  {
+    id:1,
+    question:"What is 2+2",
+    answer:"4",
+    options:['2','3','4','5']
+
+  },
+  {
+    id:2,
+    question:"What is 3+3",
+    answer:"6",
+    options:['2','3','4','5']
+
+  },
+  {
+    id:3,
+    question:"What is 4+4",
+    answer:"8",
+    options:['2','3','4','8']
+
+  },
+]
 export default App;
+
+
